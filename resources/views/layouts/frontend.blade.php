31f627c65deb6684356d5ce3b31d58f7254c36f1
Laravel Daily <info@laraveldaily.com>
2023-07-04 15:25:19 +0000

    QuickAdminPanel automatic commit

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -3,116 +3,222 @@
 
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Fastkart">
-    <meta name="keywords" content="Fastkart">
-    <meta name="author" content="Fastkart">
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Google font -->
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Exo+2:wght@400;500;600;700;800;900&display=swap"
-          rel="stylesheet">
-    <link
-        href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-        rel="stylesheet">
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-    <!-- bootstrap css -->
-    <link id="rtl-link" rel="stylesheet" type="text/css" href="{{asset("assets/css/vendors/bootstrap.css")}}">
-
-    <!-- wow css -->
-    <link rel="stylesheet" href="{{asset("assets/css/animate.min.css")}}"/>
-
-    <!-- font-awesome css -->
-    <link rel="stylesheet" type="text/css" href="{{asset("assets/css/vendors/font-awesome.css")}}">
-
-    <!-- feather icon css -->
-    <link rel="stylesheet" type="text/css" href="{{asset("assets/css/vendors/feather-icon.css")}}">
-
-    <!-- slick css -->
-    <link rel="stylesheet" type="text/css" href="{{asset("assets/css/vendors/slick/slick.css")}}">
-    <link rel="stylesheet" type="text/css" href="{{asset("assets/css/vendors/slick/slick-theme.css")}}">
-
-    <!-- Iconly css -->
-    <link rel="stylesheet" type="text/css" href="{{asset("assets/css/bulk-style.css")}}">
-    <link rel="stylesheet" type="text/css" href="{{asset("assets/css/vendors/animate.css")}}">
-
-    <!-- Template css -->
-    <link id="color-link" rel="stylesheet" type="text/css" href="{{asset("assets/css/style.css")}}">
+    <!-- Styles -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet" />
+    <link href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" rel="stylesheet" />
+    <link href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css" rel="stylesheet" />
+    <link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet" />
+    <link href="https://cdn.datatables.net/buttons/1.2.4/css/buttons.dataTables.min.css" rel="stylesheet" />
+    <link href="https://cdn.datatables.net/select/1.3.0/css/select.dataTables.min.css" rel="stylesheet" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/css/select2.min.css" rel="stylesheet" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.min.css" rel="stylesheet" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.5.1/min/dropzone.min.css" rel="stylesheet" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/jquery.perfect-scrollbar/1.5.0/css/perfect-scrollbar.min.css" rel="stylesheet" />
+    <link href="{{ asset('css/custom.css') }}" rel="stylesheet" />
     @yield('styles')
 </head>
 
-<body class="bg-effect">
+<body>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-<!-- Loader Start -->
-<div class="fullpage-loader">
-    <span></span>
-    <span></span>
-    <span></span>
-    <span></span>
-    <span></span>
-    <span></span>
-</div>
-<!-- Loader End -->
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+                        @guest
+                        @else
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('frontend.home') }}">
+                                    {{ __('Dashboard') }}
+                                </a>
+                            </li>
+                        @endguest
+                    </ul>
 
-@include("layouts.header")
-@yield("content")
-@include("layouts.footer")
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if(Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
 
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
 
+                                    <a class="dropdown-item" href="{{ route('frontend.profile.index') }}">{{ __('My profile') }}</a>
+
+                                    @can('user_management_access')
+                                        <a class="dropdown-item disabled" href="#">
+                                            {{ trans('cruds.userManagement.title') }}
+                                        </a>
+                                    @endcan
+                                    @can('permission_access')
+                                        <a class="dropdown-item ml-3" href="{{ route('frontend.permissions.index') }}">
+                                            {{ trans('cruds.permission.title') }}
+                                        </a>
+                                    @endcan
+                                    @can('role_access')
+                                        <a class="dropdown-item ml-3" href="{{ route('frontend.roles.index') }}">
+                                            {{ trans('cruds.role.title') }}
+                                        </a>
+                                    @endcan
+                                    @can('user_access')
+                                        <a class="dropdown-item ml-3" href="{{ route('frontend.users.index') }}">
+                                            {{ trans('cruds.user.title') }}
+                                        </a>
+                                    @endcan
+                                    @can('category_access')
+                                        <a class="dropdown-item" href="{{ route('frontend.categories.index') }}">
+                                            {{ trans('cruds.category.title') }}
+                                        </a>
+                                    @endcan
+                                    @can('product_access')
+                                        <a class="dropdown-item" href="{{ route('frontend.products.index') }}">
+                                            {{ trans('cruds.product.title') }}
+                                        </a>
+                                    @endcan
+                                    @can('blog_access')
+                                        <a class="dropdown-item" href="{{ route('frontend.blogs.index') }}">
+                                            {{ trans('cruds.blog.title') }}
+                                        </a>
+                                    @endcan
+                                    @can('top_label_access')
+                                        <a class="dropdown-item" href="{{ route('frontend.top-labels.index') }}">
+                                            {{ trans('cruds.topLabel.title') }}
+                                        </a>
+                                    @endcan
+                                    @can('deal_access')
+                                        <a class="dropdown-item" href="{{ route('frontend.deals.index') }}">
+                                            {{ trans('cruds.deal.title') }}
+                                        </a>
+                                    @endcan
+                                    @can('banner_access')
+                                        <a class="dropdown-item" href="{{ route('frontend.banners.index') }}">
+                                            {{ trans('cruds.banner.title') }}
+                                        </a>
+                                    @endcan
+                                    @can('banne_slider_access')
+                                        <a class="dropdown-item" href="{{ route('frontend.banne-sliders.index') }}">
+                                            {{ trans('cruds.banneSlider.title') }}
+                                        </a>
+                                    @endcan
+                                    @can('offer_access')
+                                        <a class="dropdown-item" href="{{ route('frontend.offers.index') }}">
+                                            {{ trans('cruds.offer.title') }}
+                                        </a>
+                                    @endcan
+                                    @can('special_offer_access')
+                                        <a class="dropdown-item" href="{{ route('frontend.special-offers.index') }}">
+                                            {{ trans('cruds.specialOffer.title') }}
+                                        </a>
+                                    @endcan
+                                    @can('contact_access')
+                                        <a class="dropdown-item" href="{{ route('frontend.contacts.index') }}">
+                                            {{ trans('cruds.contact.title') }}
+                                        </a>
+                                    @endcan
+                                    @can('static_page_access')
+                                        <a class="dropdown-item" href="{{ route('frontend.static-pages.index') }}">
+                                            {{ trans('cruds.staticPage.title') }}
+                                        </a>
+                                    @endcan
+
+                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <main class="py-4">
+            @if(session('message'))
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="alert alert-success" role="alert">{{ session('message') }}</div>
+                        </div>
+                    </div>
+                </div>
+            @endif
+            @if($errors->count() > 0)
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="alert alert-danger">
+                                <ul class="list-unstyled mb-0">
+                                    @foreach($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endif
+            @yield('content')
+        </main>
+    </div>
 </body>
-
-
-<!-- latest jquery-->
-<script src="{{asset("assets/js/jquery-3.6.0.min.js")}}"></script>
-
-<!-- jquery ui-->
-<script src="{{asset("assets/js/jquery-ui.min.js")}}"></script>
-
-<!-- Bootstrap js-->
-<script src="{{asset("assets/js/bootstrap/bootstrap.bundle.min.js")}}"></script>
-<script src="{{asset("assets/js/bootstrap/bootstrap-notify.min.js")}}"></script>
-<script src="{{asset("assets/js/bootstrap/popper.min.js")}}"></script>
-
-<!-- feather icon js-->
-<script src="{{asset("assets/js/feather/feather.min.js")}}"></script>
-<script src="{{asset("assets/js/feather/feather-icon.js")}}"></script>
-
-<!-- Lazyload Js -->
-<script src="{{asset("assets/js/lazysizes.min.js")}}"></script>
-
-<!-- Slick js-->
-<script src="{{asset("assets/js/slick/slick.js")}}"></script>
-<script src="{{asset("assets/js/slick/slick-animation.min.js")}}"></script>
-<script src="{{asset("assets/js/slick/custom_slick.js")}}"></script>
-
-<!-- Auto Height Js -->
-<script src="{{asset("assets/js/auto-height.js")}}"></script>
-
-<!-- Timer Js -->
-<script src="{{asset("assets/js/timer1.js")}}"></script>
-
-<!-- Fly Cart Js -->
-<script src="{{asset("assets/js/fly-cart.js")}}"></script>
-
-<!-- Quantity js -->
-<script src="{{asset("assets/js/quantity-2.js")}}"></script>
-
-<!-- WOW js -->
-<script src="{{asset("assets/js/wow.min.js")}}"></script>
-<script src="{{asset("assets/js/custom-wow.js")}}"></script>
-
-<!-- script js -->
-<script src="{{asset("assets/js/script.js")}}"></script>
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.perfect-scrollbar/1.5.0/perfect-scrollbar.min.js"></script>
+<script src="//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+<script src="//cdn.datatables.net/buttons/1.2.4/js/dataTables.buttons.min.js"></script>
+<script src="//cdn.datatables.net/buttons/1.2.4/js/buttons.flash.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.2.4/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.2.4/js/buttons.print.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.2.4/js/buttons.colVis.min.js"></script>
+<script src="https://cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/pdfmake.min.js"></script>
+<script src="https://cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/vfs_fonts.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jszip/2.5.0/jszip.min.js"></script>
+<script src="https://cdn.datatables.net/select/1.3.0/js/dataTables.select.min.js"></script>
+<script src="https://cdn.ckeditor.com/ckeditor5/16.0.0/classic/ckeditor.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/js/select2.full.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.5.1/min/dropzone.min.js"></script>
+<script src="{{ asset('js/main.js') }}"></script>
 @yield('scripts')
 
 </html>
