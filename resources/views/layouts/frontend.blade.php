1d9ff5acc8d7106d6e1f68f7bfff752164c106ec
iaxmedov <isaakahmedov@gmail.com>
2023-07-06 09:01:05 +0500

    added modal

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -48,6 +48,35 @@
     <!-- Template css -->
     <link id="color-link" rel="stylesheet" type="text/css" href="{{asset("assets/css/style.css")}}">
     @yield('styles')
+
+    @if(config("env")=="production")
+        <!-- Yandex.Metrika counter -->
+        <script type="text/javascript">
+            (function (m, e, t, r, i, k, a) {
+                m[i] = m[i] || function () {
+                    (m[i].a = m[i].a || []).push(arguments)
+                };
+                m[i].l = 1 * new Date();
+                for (var j = 0; j < document.scripts.length; j++) {
+                    if (document.scripts[j].src === r) {
+                        return;
+                    }
+                }
+                k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+            })
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(94221086, "init", {
+                clickmap: true,
+                trackLinks: true,
+                accurateTrackBounce: true,
+                webvisor: true,
+                trackHash: true,
+                ecommerce: "dataLayer"
+            });
+        </script>
+        <!-- /Yandex.Metrika counter -->
+    @endif
 </head>
 
 <body class="bg-effect">
